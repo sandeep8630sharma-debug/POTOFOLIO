ce7b4ac236740bb908e9b6adc0454108611bdf68
Sandeep Sharma <SandeepSharma@gmail.com>
2026-03-30 17:08:21 +0530

    ss01

---
+++ frontend/my-app/src/components/pagenation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function pagenation() {
+  return (
+    <div>
+        <p>my portofolio</p>
+        <div>
+           <div>
+            <p> create a new page</p>
+            <p>lead react</p>
+            <p>create a todo app</p>
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default pagenation
